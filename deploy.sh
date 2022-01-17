b5f59e2f7c1c77dbf6110a2fa820e915b3a4a601
Seculoper <69557968+seculoper235@users.noreply.github.com>
2022-01-17 10:19:44 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 echo "> 파일 압축 해제"
-# shellcheck disable=SC2164
-cd /home/ec2-user/build
-unzip SecuApp.zip
-rm -f SecuApp.zip
 
  echo "> FE 배포"
 # 제가 본 예제에서는 S3에서 EC2로 파일을 받는 폴더를 별도로 지정한 후에 복사하여 원하는 경로에 파일을 이동하는 작업을 진행한 것 같습니다.
 # 해당 코드 출처: # <https://www.sunny-son.space/AWS/Github%20Action%20CICD/>
 # 어떤 작업을 진행하는 지는 본인이 지정하면 될 것 같습니다.
 # 저는 별도의 작업을 진행하지 않았습니다.
- sudo cp -rf /home/ec2-user/build/*.jar /usr/local/tomcat9/webapps/ROOT/WEB-INF
+ sudo cp -rf /home/ec2-user/build/*.zip /usr/local/tomcat9/webapps/ROOT/WEB-INF
